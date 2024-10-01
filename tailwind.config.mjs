f4dd54d7b2fd724fa385733c30f8f3a95e850e44
mediocrest-webdev <mediocrestdev@gmail.com>
2024-10-01 21:44:57 +0800

    Improved the template and organized the folder

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,20 +4,29 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: '',   
+				text: '', //for texts
+				background: '', //background color
+				primary: '',     
 				secondary: '', 
 				accent: '',   
 			  },
 			  fontSize: {
-				'caption': ['0.75rem', { lineHeight: '1.25rem', letterSpacing: '0.1em' }],  // Small text for captions
-				'body-sm': ['0.875rem', { lineHeight: '1.5rem', letterSpacing: '0.05em' }], // Smaller body text
-				'body': ['1rem', { lineHeight: '1.75rem', letterSpacing: '0' }],           // Default body text size
-				'body-lg': ['1.125rem', { lineHeight: '1.75rem', letterSpacing: '0' }],    // Larger body text for readability
-				'heading-sm': ['clamp(1.25rem, 1.5vw + 1rem, 1.5rem)', { lineHeight: '1.5rem', letterSpacing: '0.025em' }], // Main headings
-				'heading-md': ['clamp(1.5rem, 2vw + 1rem, 2rem)', { lineHeight: '1.5rem', letterSpacing: '0' }], // Key section headings
-				'heading-lg': ['clamp(1.875rem, 2.5vw + 1rem, 2.5rem)', { lineHeight: '1.5rem', letterSpacing: '0' }], // Primary page headings
-				'heading-xl': ['clamp(2.25rem, 3vw + 1rem, 3rem)', { lineHeight: '1.5rem', letterSpacing: '0' }], // Large hero section headings
-				'headline': ['clamp(3rem, 4vw + 1rem, 4rem)', { lineHeight: '1.5rem', letterSpacing: '0.05em' }], // Biggest headlines for marketing
+				'body-sm': '.5rem', // Smaller body text
+				'body': '1rem',           // Default body text size
+				'body-lg': '1.5rem',    // Larger body text for readability
+				'heading-sm': '2rem', // Main headings
+				'heading-md': '2.5rem', // Key section headings
+				'heading-lg': '3rem', // Primary page headings
+				'heading-xl': '3.5rem', // Large hero section headings
+				'headline': '', // Biggest headlines for marketing
+			  },
+			  space: {
+				'xs':'2px',
+				'sm':'4px',
+				'md':'8px',
+				'lg':'16px',
+				'xl':'24px',
+				'2xl':'32px',
 			  },
 		},
 	},
