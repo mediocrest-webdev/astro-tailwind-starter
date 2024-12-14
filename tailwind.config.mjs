8e1bf0d8116520ae23e10739bb6a6009264e4fd9
mediocrest-webdev <mediocrestdev@gmail.com>
2024-12-14 20:11:10 +0800

    updated the template

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,28 +5,10 @@ export default {
     extend: {
       colors: {
         text: "",
-        contrast: "",
         primary: "",
         accent: "",
       },
-      fontSize: {
-        xs: "", 
-        sm: "", 
-        base: "1em", 
-        lg: "", 
-        xl: "", 
-        "2xl": "", 
-        "3xl": "", 
-        "4xl": "", 
-        "5xl": "", 
-      },
-      spacing: {
-				'section-sm': '2rem',   // Small section spacing
-				'section-md': '4rem',   // Medium section spacing
-				'section-lg': '6rem',   // Large section spacing
-				'section-xl': '8rem',   // Extra-large section spacing
-				'section-xxl': '10rem', // Extra extra-large section spacing
-			},
+
       fontFamily: {
         light: ["-Light", "sans-serif"],
         regular: ["-Regular", "sans-serif"],
